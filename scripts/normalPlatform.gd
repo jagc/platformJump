d60d323e9b43dc137bc0937476ffb8c10de88a82
jagc <jag.cordoviz@gmail.com>
2019-05-20 00:29:39 +0800

    created grass platform with it's scripts

---
+++ scripts/normalPlatform.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("body_entered", self, "_on_body_entered")
+
+func _on_body_entered(body):
+	if body.name == "player":
+		if body.position.y < position.y:
+			body.jump()
