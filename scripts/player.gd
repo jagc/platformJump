710799d2ca728bf25991407243ff9537b5579d32
jagc <jag.cordoviz@gmail.com>
2019-05-20 00:38:45 +0800

    setup player script variables

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,19 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var animated_sprite = $AnimatedSprite
 
-# Called when the node enters the scene tree for the first time.
+const GRAVITY = 1500
+const GRAVITY_INCREMENT = 2500
+const JUMP_DECREMENT = 100
+
+var screen_width
+var half_sprite_width
+
+var jumping = false
+var current_jump_force = 0
+var current_gravity = 0
+
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	screen_width = get_viewport_rect().size.x
+	half_sprite_width = animated_sprite.frames.get_frame("idle", 0).get_width() / 2
+	current_gravity = GRAVITY
