47475f1013e0d0fe435a8196d36385c4db1e4ab1
jagc <jag.cordoviz@gmail.com>
2019-05-23 14:22:43 +0800

    wrapped previous commit into a logic. Because code neatness

--- scripts/game.gd
+++ scripts/game.gd
@@ -9,6 +9,8 @@ onready var score_text = $ui/score
 const MIN_INTERVAL = 100
 const MAX_INTERVAL = 250
 const INITIAL_PLATFORMS_COUNT = 10
+const SPECIAL_PLATFORMS_CHANCE_CAP = 90
+const SPECIAL_PLATFORMS_CHANCE_INCREMENT = 5
 
 var special_platforms_chance = 0
 var current_max_interval
@@ -56,19 +58,20 @@ func _spawn_platform():
 	current_min_interval = clamp(current_min_interval, MIN_INTERVAL, MAX_INTERVAL / 0.75)
 
 func on_platform_just_exited():
+	_platform_chance_logic()
+	_spawn_caller()
+
+func _spawn_caller():
+	call_deferred("_spawn_platform")
+	
+func _platform_chance_logic():
 	if special_platforms_chance == 0:
-		special_platforms_chance = 5
-	elif special_platforms_chance <= 90:
+		special_platforms_chance = SPECIAL_PLATFORMS_CHANCE_INCREMENT
+	elif special_platforms_chance <= SPECIAL_PLATFORMS_CHANCE_CAP:
 		if player.score >= player_score_checkpoint:
 			is_player_score_checkpoint = true
 			player_score_checkpoint += player.score
 		else:
 			is_player_score_checkpoint = false
 		if is_player_score_checkpoint:
-			special_platforms_chance += 5
-			print('added special platform chance.' + 'plat chance at: ' + str(special_platforms_chance))
-		
-	_spawn_caller()
-
-func _spawn_caller():
-	call_deferred("_spawn_platform")
+			special_platforms_chance += SPECIAL_PLATFORMS_CHANCE_INCREMENT
