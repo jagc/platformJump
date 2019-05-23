c76039850c7b0e2d609cd6a7bd103241324d7df4
jagc <jag.cordoviz@gmail.com>
2019-05-23 14:19:45 +0800

    added feature where spawning special platforms increments as the player's score goes up.

--- scripts/game.gd
+++ scripts/game.gd
@@ -9,12 +9,14 @@ onready var score_text = $ui/score
 const MIN_INTERVAL = 100
 const MAX_INTERVAL = 250
 const INITIAL_PLATFORMS_COUNT = 10
-const SPECIAL_PLATFORM_CHANCE = 100
 
+var special_platforms_chance = 0
 var current_max_interval
 var current_min_interval
 var last_spawn_height
 var screen_size
+var player_score_checkpoint = 1000
+var is_player_score_checkpoint = false
 
 func _ready():
 	levelManager.current_scene_name = self.get_name()
@@ -35,7 +37,7 @@ func _spawn_platform():
 	randomize()
 	var index
 	var new_platform
-	if rand_range(0, 100) > 100 - SPECIAL_PLATFORM_CHANCE:
+	if rand_range(0, 100) > 100 - special_platforms_chance:
 		index = rand_range(0, special_platforms.size())
 		new_platform = special_platforms[index].instance()
 	else:
@@ -54,6 +56,18 @@ func _spawn_platform():
 	current_min_interval = clamp(current_min_interval, MIN_INTERVAL, MAX_INTERVAL / 0.75)
 
 func on_platform_just_exited():
+	if special_platforms_chance == 0:
+		special_platforms_chance = 5
+	elif special_platforms_chance <= 90:
+		if player.score >= player_score_checkpoint:
+			is_player_score_checkpoint = true
+			player_score_checkpoint += player.score
+		else:
+			is_player_score_checkpoint = false
+		if is_player_score_checkpoint:
+			special_platforms_chance += 5
+			print('added special platform chance.' + 'plat chance at: ' + str(special_platforms_chance))
+		
 	_spawn_caller()
 
 func _spawn_caller():
